1ca4bc32e60575611f4f89687841bc07ee2d191a
MelinaCir <mc222ap@student.lnu.se>
2020-01-03 14:27:43 +0100

    Updating docker compose file

--- part1/microservice-demo-flask-master/src/app.py
+++ part1/microservice-demo-flask-master/src/app.py
@@ -7,5 +7,5 @@ def hello_world():
     return jsonify({"message": 'Hello from flask, Im python'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
